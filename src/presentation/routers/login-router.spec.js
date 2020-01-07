c178f150729819e866e0ceb226db60860e729bfc
italolourenco <italolt10@gmail.com>
2020-01-07 09:06:43 -0300

    test

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,3 +1,23 @@
-describe("", () => {
-  test("", () => {});
-});
+class Loginrouter {
+  route (httpRequest) {
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400
+      }
+    }
+  }
+}
+
+describe('Login Router', () => {
+  test('Should return 400 if no email is provided', () => {
+    const sut = new Loginrouter()
+    const httpRequest = {
+      body: {
+        password: '123'
+      }
+    }
+
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
